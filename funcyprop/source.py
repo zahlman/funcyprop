c081dea5ddf96f00d3aa80698f5b47660b3a2390
Karl Knechtel <karl.a.knechtel@gmail.com>
2022-01-20 07:20:59 -0500

    fold classes back together

--- funcyprop/source.py
+++ funcyprop/source.py
@@ -3,18 +3,16 @@ t = _symbols('t')
 del _symbols
 
 
-class PiecewiseBuilderT:
-    def __init__(self, clocktype=float):
+class Source:
+    def __init__(self, clock, clocktype=float, resulttype=float):
+        self._clock = clock
         self._clocktype = clocktype
+        self._resulttype = resulttype
         self._funcs = [sympify(clocktype())]
         self._conditions = [True]
         self._loop = None
         self._end = clocktype()
-        self._built = None
-
-
-    def _recalculate(self):
-        self._built = Piecewise(*zip(self._funcs, self._conditions))
+        self._formula = None
 
 
     @property
@@ -29,21 +27,8 @@ class PiecewiseBuilderT:
         self._loop = value
 
 
-    @property
-    def loopsize(self):
-        l = self.loop
-        return None if l is None else self._end - l
-
-
-    @property
-    def built(self):
-        if self._built is None:
-            self._recalculate()
-        return self._built
-
-
     def add(self, func, duration):
-        self._built = None # invalidate cache
+        self._formula = None # invalidate cache
         # rebase time to the start of the segment.
         self._funcs.insert(-1, func.subs(t, t-self._end))
         self._end += duration
@@ -51,39 +36,24 @@ class PiecewiseBuilderT:
         self._conditions.insert(-1, t < self._end)
 
 
-class Source:
-    def __init__(self, clock, clocktype=float, resulttype=float):
-        self._clock = clock
-        self._resulttype = resulttype
-        self._builder = PiecewiseBuilderT(clocktype)
+    def reset(self, now=0):
+        # allow recalculation after adding but before value grab, to
+        # potentially avoid hiccups
+        self.formula
+        self._clock.now = now
 
 
     @property
     def formula(self):
-        return self._builder.built
-
-
-    @property
-    def loop(self):
-        return self._builder.loop
-
-
-    @loop.setter
-    def loop(self, value):
-        self._builder.loop = value
-
-
-    def reset(self, now=0):
-        self._clock.now = now
+        # also expose this as read-only, for easier testing.
+        if self._formula is None:
+            self._formula = Piecewise(*zip(self._funcs, self._conditions))
+        return self._formula
 
 
     @property
     def value(self):
         now, l = self._clock.now, self.loop
         if l is not None:
-            now = l + (now - l) % self._builder.loopsize
+            now = l + (now - l) % (self._end - l)
         return self._resulttype(self.formula.subs(t, now))
-
-
-    def add(self, func, duration):
-        self._builder.add(func, duration)
